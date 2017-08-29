68a2bdfaf147af10f5648392d996bf62d9e50c9e
JOKER\Joker <goodsave@qq.com>
2017-08-29 21:08:32 +0800

    初提交

---
+++ minsx-starter/src/main/java/com/minsx/starter/ApplicationStarter.java
@@ -0,0 +1,20 @@
+package com.minsx.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ApplicationStarter
+ * Created by Joker on 2017/8/29.
+ */
+@SpringBootApplication
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApplicationStarter.class, args);
+
+    }
+
+
+}
