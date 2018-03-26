0642d807874afb70a8f08e9ba7757b98e260538f
JOKER\Joker <goodsave@qq.com>
2018-03-26 20:15:36 +0800

    新增Executor

--- minsx-framework-common/minsx-framework-common-basic/src/test/java/com/minsx/common/basic/SimpleTest.java
+++ minsx-framework-common/minsx-framework-common-basic/src/test/java/com/minsx/common/basic/SimpleTest.java
@@ -18,16 +18,37 @@
 package com.minsx.common.basic;
 
 import com.minsx.framework.common.basic.StringUtil;
+import com.minsx.framework.common.basic.executor.Executor;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class SimpleTest {
 
     @Test
     public void testSimple() {
-        String value = StringUtil.getParamFromUrl("https://blog.csdn.net/article?name=goodsave&age=25&sex=man","age");
+        String value = StringUtil.getParamFromUrl("https://blog.csdn.net/article?name=goodsave&age=25&sex=man", "age");
         System.out.println(value);
-        StringUtil.parseUrlParams("https://blog.csdn.net/article?name=goodsave&age=25&sex=man").forEach((k,v)->System.out.println(k+":"+v));
+        StringUtil.parseUrlParams("https://blog.csdn.net/article?name=goodsave&age=25&sex=man").forEach((k, v) -> System.out.println(k + ":" + v));
     }
 
+    @Test
+    public void testExecutor() throws InterruptedException {
+        Executor executor = new Executor();
+        executor.script("java -jar minsx-authorization-starter-1.0.0.jar -Duser.timezone=GMT+08")
+                .inPath("E:/Temp/ServerRunner/MsAuthServer")
+                .charset("UTF-8")
+                .bufferSize(20480)
+                .sync(false)
+                .output(false)
+                .errorLineHandler(line -> {
+                    System.out.println("ERROR: " + line);
+                }).normalLineHandler(line -> {
+                    System.out.println("NORMAL: " + line);
+                }).execute();
+
+        Thread.sleep(25000);
+        executor.stop();
+    }
 
 }
